d746b54826c9dedc4d7a15829b986b98ff5233f2
Aaron E-J <the@otherrealm.org>
2019-10-28 20:04:43 -0400

    Have basic layout done

--- jsonGen.js
+++ jsonGen.js
@@ -1,137 +1,62 @@
+function placement(n){
+	var xy=[];
+	for(var i=0;i<n;i++){
+		xy.push({x:(Math.cos(((2*i*Math.PI)/n))*300),y:(Math.sin(((2*i*Math.PI)/n))*300)});
+	}
+	return xy;
+}
 function rand(n) { return Math.random() * n; };
-function createAofInts(q) {
+const createAofInts = q => {
 	var a = [];
 	for (var i = 0; i < q; i++) {
 		a.push(i);
 	}
 	return a;
 }
-var nids = createAofInts(50);
+const nids = 40;
+var iterIds = 0;
 console.log(nids);
-var jsonNode = {
-	group: 'nodes',
-	data: { // element data (put json serialisable dev data here)
-		id: 0,
-		money: rand(100000),
-		width: this.money * .001,
-		height: this.money * .001,
-	},
-	position: { // the model position of the node (optional on init, mandatory after)
-		x: 100,
-		y: 100,
-	},
-	selected: false, // whether the element is selected (default false)
-	selectable: true, // whether the selection state is mutable (default true)
-	locked: false, // when locked a node's position is immutable (default false)
-	grabbable: true, // whether the node can be grabbed and moved by the user
-	pannable: false, // whether dragging the node causes panning instead of grabbing
-};
-var jsonEdge = {
-	group: 'edges',
-	data: {
-		id: 0,
-		source: 'buffer', // the source node id (edge comes from this node)
-		target: nids[0]
-	},
-	pannable: true // whether dragging on the edge causes panning
-};
+function jsonNode(id) {
+	var m=rand(100000);
+	var n = {
+		group: 'nodes',
+		data: { // element data (put json serialisable dev data here)
+			id: id,
+			money: m,
+			width: m * .0005,
+			height: m * .0005,
+		},
+		position: { // the model position of the node (optional on init, mandatory after)
+			x: 100,
+			y: 100,
+		},
+		selected: false, // whether the element is selected (default false)
+		selectable: true, // whether the selection state is mutable (default true)
+		locked: false, // when locked a node's position is immutable (default false)
+		grabbable: true, // whether the node can be grabbed and moved by the user
+		pannable: false, // whether dragging the node causes panning instead of grabbing
+	}
+	return n;
+}
+function jsonEdge() {
+	var e = {
+		group: 'edges',
+		data: {
+			id: 0,
+			source: 'buffer', // the source node id (edge comes from this node)
+			target: nids[0]
+		},
+		pannable: true // whether dragging on the edge causes panning
+	}
+	return e;
+}
 var cD = [];
-for (var i = 0; i < nids.length; i++) {
-	var pos = (i * 75);
-	cD.push(jsonNode);
-	cD[i].data.id = nids[i];
+var pos = placement(nids);
+for (var i = 0; i < nids; i++) {
+	cD.push(jsonNode(i));
+	cD[i].data.id = i;
+	cD[i].position.x = pos[i].x;
+	cD[i].position.y = pos[i].y;
 }
-
-if (pos <= 500) {
-	cD[i].position.x = pos % 500
-}
-
-var x = 0,
-	y = 0,
-	delta = [0, -1],
-	width = 5,
-	height = 3;
-
-
-for (i = Math.pow(Math.max(width, height), 2); i > 0; i--) {
-	if ((-width / 2 < x && x <= width / 2)
-		&& (-height / 2 < y && y <= height / 2)) {
-		console.debug('POINT', x, y);
-		$('#result').append('[' + x + ', ' + y + '], ');
-	}
-
-	if (x === y
-		|| (x < 0 && x === -y)
-		|| (x > 0 && x === 1 - y)) {
-		// change direction
-		delta = [-delta[1], delta[0]]
-	}
-
-	x += delta[0];
-	y += delta[1];
-}
-// 2D vectors
-const add = ([x0, y0]) => ([x1, y1]) => [x0 + x1, y0 + y1];
-const rotate = θ => ([x, y]) => [
-	Math.round(x * Math.cos(θ) - y * Math.sin(θ)),
-	Math.round(x * Math.sin(θ) + y * Math.cos(θ))
-];
-// Iterables
-const fromGen = g => ({ [Symbol.iterator]: g });
-const range = n => [...Array(n).keys()];
-const map = f => it =>
-	fromGen(function* () {
-		for (const v of it) {
-			yield f(v);
-		}
-	});
-const take = n => it =>
-	fromGen(function* () {
-		for (let v of it) {
-			if (--n < 0) break;
-			yield v;
-		}
-	});
-const empty = [];
-const append = it1 => it2 =>
-	fromGen(function* () {
-		yield* it1;
-		yield* it2;
-	});
-
-// Outward spiral
-const spiralOut = i => {
-	const n = Math.floor(i / 2) + 1;
-	const leg = range(n).map(x => [x, 0]);
-	const transform = p => add([n, 0])(rotate(Math.PI / 2)(p));
-
-	return fromGen(function* () {
-		yield* leg;
-		yield* map(transform)(spiralOut(i + 1));
-	});
-};
-
-// Test
-{
-	const points = [...take(5)(spiralOut(0))];
-	console.log(JSON.stringify(points));
-}
-
-// Inward spiral
-const spiralIn = ([w, h]) => {
-	const leg = range(w).map(x => [x, 0]);
-	const transform = p => add([w - 1, 1])(rotate(Math.PI / 2)(p));
-
-	return w * h === 0
-		? empty
-		: append(leg)(
-			fromGen(function* () {
-				yield* map(transform)(spiralIn([h - 1, w]));
-			})
-		);
-};
-// Test
-{
-	const points = [...spiralOut([5, 5])];
-	console.log(JSON.stringify(points));
-}
+console.log(pos,cD);
+ JSON.parse(cD);
--- ord.js
+++ ord.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	$("#cy").css('height', h);
 	var nw = ['100px', '200px', '50px'];
 	var iter = 0;
+	var cy = [];
 	var bufferF = 1000000;
 	function rand(n) { return Math.random() * n; };
 	function arrayONumbers(q, rang) {
@@ -16,18 +17,79 @@ document.addEventListener('DOMContentLoaded', function () {
 		console.log(q, rang, a);
 		return a;
 	}
-	fetch('fakeData.json', { mode: 'no-cors' })
-		.then(function (res) {
-			return res.json()
-		})
-		.then(function (data) {
-			var cy = cytoscape({
+	var fakeData = new Promise(function (resolve, reject) {
+		function placement(n) {
+			var xy = [];
+			for (var i = 0; i < n; i++) {
+				xy.push({ x: (Math.cos(((2 * i * Math.PI) / n)) * 300), y: (Math.sin(((2 * i * Math.PI) / n)) * 300) });
+			}
+			return xy;
+		}
+		function rand(n) { return Math.random() * n; };
+		const nids = 40;
+		console.log(nids);
+		function jsonNode(id) {
+			var m = rand(100000);
+			var n = {
+				group: 'nodes',
+				data: { // element data 
+					id: 'n'+id,
+					money: m,
+					width: m * .0005,
+					height: m * .0005,
+				},
+				position: { // the model position of the node 
+					x: 100,
+					y: 100,
+				},
+				selected: false, // whether the element is selected 
+				selectable: true, // whether the selection state is mutable 
+				locked: false, // when locked a node's position is immutable 
+				grabbable: true, // whether the node can be grabbed and moved by the user
+				pannable: false, // whether dragging the node causes panning instead of grabbing
+			}
+			return n;
+		}
+		function jsonEdge(id) {
+			var e = {
+				group: 'edges',
+				data: {
+					id: 'e'+id,
+					source: 'nbuffer', // the source node id (edge comes from this node)
+					target: 'n'+id
+				},
+				pannable: true // whether dragging on the edge causes panning
+			}
+			return e;
+		}
+		var cD = [];
+		var pos = placement(nids);
+		for (var i = 0; i < nids; i++) {
+			cD.push(jsonNode(i));
+			cD[i].position.x = pos[i].x;
+			cD[i].position.y = pos[i].y;
+		}
+		for (var i = 0; i < nids; i++) {
+			cD.push(jsonEdge(i));
+		}
+		var nOfObj = cD.push(jsonNode('buffer'));
+		cD[nOfObj-1].position.x = 0;
+		cD[nOfObj-1].position.y = 0;
+		cD[nOfObj-1].data.money=1000000;
+		cD[nOfObj-1].data.width=1000000;
+		cD[nOfObj-1].data.height=1000000;
+		console.log(JSON.stringify(cD))
+		resolve(cD);
+	});
+	fakeData
+		.then((data) => {
+			console.log(data);
+			cy = cytoscape({
 				container: document.getElementById('cy'),
 				elements: data,
 				layout: {
 					name: 'preset'
 				},
-
 				style: cytoscape.stylesheet()
 					.selector('node')
 					.css({
@@ -41,31 +103,36 @@ document.addEventListener('DOMContentLoaded', function () {
 					.css({
 						'height': 'data(height)'
 					})
-					
+					.selector('edge[width]')
+					.css({
+						'width': '1px'
+					})
+
 			});
 			return data;
-		}).then(function(data){
-			cy.add([ // flat array of nodes and edges
-				{
-					group: 'nodes', // 'nodes' for a node, 'edges' for an edge
-					// NB the group field can be automatically inferred for you but specifying it
-					// gives you nice debug messages if you mis-init elements
-					data: { // element data (put json serialisable dev data here)
-						id: 'buffer', // mandatory (string) id for each element, assigned automatically on undefined
-						width: bufferF * 0.000001,
-						height: bufferF * 0.000001,
-					},
-					position: { // the model position of the node (optional on init, mandatory after)
-						x: w / 2,
-						y: h / 2,
-					},
-					selected: false, // whether the element is selected (default false)
-					selectable: true, // whether the selection state is mutable (default true)
-					locked: false, // when locked a node's position is immutable (default false)
-					grabbable: true, // whether the node can be grabbed and moved by the user
-					pannable: false, // whether dragging the node causes panning instead of grabbing
-					classes: ['foo', 'bar'] // an array (or a space separated string) of class names that the element has
-				}
-			])
 		})
+	// .then(function (data) {
+	// 	cy.add( // flat array of nodes and edges
+	// 		{
+	// 			group: 'nodes', // 'nodes' for a node, 'edges' for an edge
+	// 			// NB the group field can be automatically inferred for you but specifying it
+	// 			// gives you nice debug messages if you mis-init elements
+	// 			data: { // element data (put json serialisable dev data here)
+	// 				id: 'buffer', // mandatory (string) id for each element, assigned automatically on undefined
+	// 				width: bufferF * 0.000001,
+	// 				height: bufferF * 0.000001,
+	// 			},
+	// 			position: { // the model position of the node (optional on init, mandatory after)
+	// 				x: w / 2,
+	// 				y: h / 2,
+	// 			},
+	// 			selected: false, // whether the element is selected (default false)
+	// 			selectable: true, // whether the selection state is mutable (default true)
+	// 			locked: false, // when locked a node's position is immutable (default false)
+	// 			grabbable: true, // whether the node can be grabbed and moved by the user
+	// 			pannable: false, // whether dragging the node causes panning instead of grabbing
+	// 			classes: ['foo', 'bar'] // an array (or a space separated string) of class names that the element has
+	// 		}
+	// 	)
+	// })
 });
